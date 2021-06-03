f85d3707e6a6d6c32e896b81c7f157fe44a21b48
leahzeisner <zeisner.l@northeastern.edu>
2021-06-03 12:59:23 -0400

    Initial commit

---
+++ book_outlet/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.db.models.fields import CharField
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField()
